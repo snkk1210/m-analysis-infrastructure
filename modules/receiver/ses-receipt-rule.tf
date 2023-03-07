f7c71689830b33186f3ee340e7a9f698b6b2fe3c
keisukesanuki <n41210guitar@gmail.com>
2023-03-08 03:12:56 +0900

    fmt

--- modules/receiver/ses-receipt-rule.tf
+++ modules/receiver/ses-receipt-rule.tf
@@ -3,16 +3,16 @@ resource "aws_ses_receipt_rule_set" "receiver" {
 }
 
 resource "aws_ses_receipt_rule" "receiver" {
-  name = "${var.project}-${var.environment}-ses-rule"
+  name          = "${var.project}-${var.environment}-ses-rule"
   rule_set_name = aws_ses_receipt_rule_set.receiver.rule_set_name
-  recipients = ["${var.receiver_domain}"]
-  enabled = true
-  scan_enabled = true
+  recipients    = ["${var.receiver_domain}"]
+  enabled       = true
+  scan_enabled  = true
 
   sns_action {
     topic_arn = aws_sns_topic.receiver.arn
-    position = 1
-    encoding = "Base64"
+    position  = 1
+    encoding  = "Base64"
   }
 }
 
