221e1aacdd563eff7c67d1fcb653833b2a442449
snkk1210 <n41210guitar@gmail.com>
2023-03-08 10:36:52 +0900

    add

---
+++ modules/receiver/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+    type = string
+    default = ""
+}
+
+variable "environment" {
+    type = string
+    default = ""
+}
+
+variable "receiver_domain" {
+    type = string
+    default = ""
+}
