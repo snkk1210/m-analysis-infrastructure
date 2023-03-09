0dc5841aba22ea3ffdf65fb6070b2ffe9b268965
snkk1210 <n41210guitar@gmail.com>
2023-03-09 20:21:22 +0900

    modify

--- modules/receiver/lambda/source/receiver.py
+++ modules/receiver/lambda/source/receiver.py
@@ -11,3 +11,5 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
 
     logger.info("Event: " + str(event))
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    logger.info("Message: " + str(message))
