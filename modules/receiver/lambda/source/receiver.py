c0e4e30fc6944ffedd254108d238ff7d6d9c5316
snkk1210 <n41210guitar@gmail.com>
2023-03-10 15:49:14 +0900

    modify

--- modules/receiver/lambda/source/receiver.py
+++ modules/receiver/lambda/source/receiver.py
@@ -13,26 +13,13 @@ def lambda_handler(event, context):
     logger.info("Event: " + str(event))
     
     message = json.loads(event['Records'][0]['Sns']['Message'])
-    logger.info("Message: " + str(message))
-
     mail = message['mail']
-    logger.info("Mail: " + str(mail))
 
     timestamp = message['mail']['timestamp']
-    logger.info("Timestamp: " + str(timestamp))
-
     m_from = message['mail']['commonHeaders']['from']
-    logger.info("From: " + str(m_from))
-
     date = message['mail']['commonHeaders']['date']
-    logger.info("Date: " + str(date))
-
     subject = message['mail']['commonHeaders']['subject']
-    logger.info("Subject: " + str(subject))
-
     content = message['content']
-    logger.info("Content: " + str(content))
-
 
     email_obj = email.message_from_string(content)
 
@@ -62,7 +49,16 @@ def lambda_handler(event, context):
         logger.info("There is Attach File")
         body += "Error: Attachments are not supported"
 
-    logger.info("AttachData: " + str(attach_data))
+    # NOTE: Logging
+    logger.info("Message: " + str(message))
+    logger.info("Mail: " + str(mail))
+    logger.info("Timestamp: " + str(timestamp))
+    logger.info("From: " + str(m_from))
+    logger.info("Date: " + str(date))
+    logger.info("Subject: " + str(subject))
+    logger.info("Content: " + str(content))
+
+    logger.info("Attachment: " + str(attach_data))
+    logger.info("Body: " + str(body))
     logger.info("Text: " + str(body_text))
     logger.info("HTML: " + str(body_html))
-    logger.info("Body: " + str(body))
